7b1fb95632b5f332bfb8091051a19400868add68
Anurag p <87142203+abhijithp327@users.noreply.github.com>
2023-11-18 14:35:17 +0530

    new changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from '../../assets/images/logo.png';
 import { NavLink, Link } from 'react-router-dom';
+import userImg from '../../assets/images/avatar-icon.png';
+import {BiMenu} from 'react-icons/bi';
 
 
 const navLinks = [
@@ -54,6 +56,28 @@ const Header = () => {
                 ))
               }
             </ul>
+          </div>
+
+          {/* nav right */}
+
+          <div className='flex items-center gap-4'>
+                <div>
+                  <Link to='/'>
+                    <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
+                      <img src={userImg} className='w-full rounded-full' alt="userimg" />
+                    </figure>
+                  </Link>
+                </div>
+
+                <Link to='/login'>
+                  <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>
+                    Login</button>
+                </Link>
+
+                <span className='md:hidden'>
+                  <BiMenu className='w-6 h-6 cursor-pointer'/>
+                </span>
+
           </div>
 
         </div>

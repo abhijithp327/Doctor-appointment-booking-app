4703411ded77978da918ace856ef9299ba6fe3c6
Anurag p <87142203+abhijithp327@users.noreply.github.com>
2023-11-19 12:44:50 +0530

    hero section commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import heroImg01 from "../assets/images/hero-img01.png";
+import heroImg02 from "../assets/images/hero-img02.png";
+import heroImg03 from "../assets/images/hero-img03.png";
+
+
+
 
 const Home = () => {
   return (
     <>
       {/* hero section */}
 
-    <>
+    
     <section className="hero__section pt-[60px] 2xl:h-[800px]">
       <div className="container">
         <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
@@ -22,14 +28,56 @@ const Home = () => {
 
                  <button className="btn">Request an Appointment</button>
             </div>
+
+            {/* hero counter */}
+            <div className='mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]'>
+              <div>
+                <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>30+</h2>
+                <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                <p className="text__para">Years of Experience</p>
+              </div>
+
+
+              <div>
+                <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>15+</h2>
+                <span className='w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]'></span>
+                <p className="text__para">Clinic Location</p>
+              </div>
+
+
+              <div>
+                <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>100%</h2>
+                <span className='w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]'></span>
+                <p className="text__para">Patient Satisfaction</p>
+              </div>
+
+
+            </div>
+
           </div>
+
+
+          {/* HERO CONTENT */}
+
+          <div className='flex gap-[30px] justify-end'>
+             <div>
+              <img src={heroImg01} alt="heroimg1" />
+             </div>
+
+             <div className='mt-[30px]'>
+              <img src={heroImg02} className='w-full mb-[30px]' alt="heroing2" />
+              <img src={heroImg03} className='w-full' alt="heroing3" />
+             </div>
+          </div>
+
+
         </div>
       </div>
     </section>
     
     </>
 
-    </>
+   
   );
 }
 

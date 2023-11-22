a67d928ac96ee78c629d29b9170c4afc87213d5b
Anurag p <87142203+abhijithp327@users.noreply.github.com>
2023-11-22 13:35:10 +0530

    Backend added

---
+++ backend/Controller/authController.js
@@ -0,0 +1,22 @@
+
+
+
+
+export const register = async (req,res) => {
+    try {
+
+    } catch (err) {
+
+    }
+};
+
+
+
+
+export const login = async (req,res) => {
+    try {
+
+    } catch (err) {
+        
+    }
+};

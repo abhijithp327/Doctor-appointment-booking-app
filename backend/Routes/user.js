bb378f7f572da011c4d468b274d755f1f0724556
Anurag p <87142203+abhijithp327@users.noreply.github.com>
2023-11-23 12:29:34 +0530

    added some files

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { deleteUser, getAllUser, getSingleUser, updateUser } from '../Controller/userController.js';
+import { authenticate, restrict } from '../auth/verifyToken.js';
 
 
 
 const router = express.Router();
 
 
-router.get('/:id', getSingleUser); 
+router.get('/:id', authenticate, restrict(['admin']), getSingleUser); 
 router.get('/', getAllUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);

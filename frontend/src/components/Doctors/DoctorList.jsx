73392ae4e55b9018b6977d76534836053c3394ef
Abhijith P <87142203+abhijithp327@users.noreply.github.com>
2024-01-03 10:27:52 +0530

    Update DoctorList.jsx

--- frontend/src/components/Doctors/DoctorList.jsx
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {doctors} from './../../assets/data/doctors';
 import DoctorCard from './DoctorCard';
 
-
 const DoctorList = () => {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
@@ -14,5 +13,4 @@ const DoctorList = () => {
         </div>
   )
 }
-
 export default DoctorList

5fbb508cdf302fb0ae2d1fcaf4a28b928e9cdd26
Abhijith P <87142203+abhijithp327@users.noreply.github.com>
2024-01-03 10:28:59 +0530

    Update DoctorCard.jsx

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -3,8 +3,6 @@ import starIcon from '../../assets/images/Star.png';
 import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs';
 
-
-
 const DoctorCard = ({ doctor }) => {
     
     const { name, avgRating, totalRating, photo, specialty, totalPatients, hospital } = doctor;

1ed474f2589b01cad2c9f27fee9db2fc5a277242
Abhijith P <87142203+abhijithp327@users.noreply.github.com>
2024-01-22 10:24:40 +0530

    Update Doctors.jsx

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -20,7 +20,6 @@ const Doctors = () => {
         </div>
     </section>
 
-
     <section>
       <div className="container">
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
